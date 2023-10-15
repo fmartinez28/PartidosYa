d911aa3b96b70c1dd540e33b42337de71d312956
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2023-10-15 00:49:47 -0300

    ultimos tests de canchas

--- api/test/routes/canchas/index.test.js
+++ api/test/routes/canchas/index.test.js
@@ -118,3 +118,169 @@ test("POST de una cancha sin nombre y con malos ids no funciona", async (t) => {
 
     t.equal(res.statusCode, 400);
 });
+
+
+test("PUT de una cancha funciona", async (t) => {
+    const app = await build(t);
+    await begin();
+
+    await app.inject({
+        url: '/direcciones',
+        method: 'POST',
+        payload: {
+            pais: "Argentina",
+            estado: "Buenos Aires",
+            ciudad: "La Plata",
+            calle: "Calle de prueba",
+            numero: 123
+        }
+    })
+
+    await app.inject({
+        url: '/telefonos',
+        method: 'POST',
+        payload: {
+            codpais: "54",
+            codarea: "221",
+            numero: "1234567"
+        }
+    })
+
+    await app.inject({
+        url: '/usuarios',
+        method: 'POST',
+        payload: {
+            nombre: "Usuario de prueba",
+            apellido: "Apellido de prueba",
+            fechanac: "2020-01-01",
+            telefonoid: 1,
+            direccionid: 1
+        }
+    })
+
+    await app.inject({
+        url: '/propietarios',
+        method: 'POST',
+        payload: {
+            usuarioid: 1
+        }
+    })
+
+    await app.inject({
+        url: '/canchas',
+        method: 'POST',
+        payload: {
+            nombre: "Cancha de prueba",
+            direccionid: 1,
+            canchanum: 1,
+            propietarioid: 1
+        }
+    });
+
+    const res = await app.inject({
+        url: '/canchas/1',
+        method: 'PUT',
+        payload: {
+            id: 1,
+            nombre: "Cancha de prueba 2",
+            direccionid: 1,
+            canchanum: 1,
+            propietarioid: 1
+        }
+    });
+
+    t.teardown(async () => {
+        await rollback();
+    });
+
+    t.equal(res.statusCode, 200);
+});
+
+test("PUT de una cancha con id de cuerpo y parámetro diferentes no funciona", async (t) => {
+    const app = await build(t);
+    await begin();
+    const res = await app.inject({
+        url: '/canchas/1',
+        method: 'PUT',
+        payload: {
+            id: 2,
+            nombre: "Cancha de prueba 2",
+            direccionid: 1,
+            canchanum: 1,
+            propietarioid: 1
+        }
+    });
+    t.teardown(async () => {
+        await rollback();
+    });
+    t.equal(res.statusCode, 409);
+});
+
+test("DELETE de una cancha funciona", async (t) => {
+    const app = await build(t);
+    await begin();
+
+    await app.inject({
+        url: '/direcciones',
+        method: 'POST',
+        payload: {
+            pais: "Argentina",
+            estado: "Buenos Aires",
+            ciudad: "La Plata",
+            calle: "Calle de prueba",
+            numero: 123
+        }
+    })
+
+    await app.inject({
+        url: '/telefonos',
+        method: 'POST',
+        payload: {
+            codpais: "54",
+            codarea: "221",
+            numero: "1234567"
+        }
+    })
+
+    await app.inject({
+        url: '/usuarios',
+        method: 'POST',
+        payload: {
+            nombre: "Usuario de prueba",
+            apellido: "Apellido de prueba",
+            fechanac: "2020-01-01",
+            telefonoid: 1,
+            direccionid: 1
+        }
+    })
+
+    await app.inject({
+        url: '/propietarios',
+        method: 'POST',
+        payload: {
+            usuarioid: 1
+        }
+    })
+
+    await app.inject({
+        url: '/canchas',
+        method: 'POST',
+        payload: {
+            nombre: "Cancha de prueba",
+            direccionid: 1,
+            canchanum: 1,
+            propietarioid: 1
+        }
+    });
+
+    const res = await app.inject({
+        url: '/canchas/1',
+        method: 'DELETE'
+    });
+
+    t.teardown(async () => {
+        await rollback();
+    });
+
+    t.equal(res.statusCode, 200);
+});
