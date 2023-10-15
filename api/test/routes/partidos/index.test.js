86070dea2488216bf4908d8fcc1aa469d5e6e688
Facundo Martínez <facusml16@gmail.com>
2023-10-15 03:20:00 -0300

    Fixeados errores SIGTERM debido a timeout (30s default), incrementado a 100s, fix partidos

--- api/test/routes/partidos/index.test.js
+++ api/test/routes/partidos/index.test.js
@@ -21,11 +21,19 @@ test("GET de todos los partidos, no hay partidos", async (t) => {
 })
 test("GET de todos los partidos", async (t) => {
     const app = await build(t);
+    await normalize.begin();
+
+    const created = await query('INSERT INTO "partido" ("canchaid", "fechacreacion", "fechaprogramada", "comunidadid") VALUES ($1, $2, $3, $4) RETURNING *;', [1, '2020-01-01', '2020-01-01', 1]);
+    const createdResId = created.rows[0].id;
 
     const res = await app.inject({
         url: '/partidos',
         method: 'GET'
     });
+
+    t.teardown(async () => {
+        await normalize.rollback();
+    })
     t.equal(res.statusCode, 200);
 })
 
