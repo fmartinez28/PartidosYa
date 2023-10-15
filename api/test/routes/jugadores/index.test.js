b395be3dc27c550930c23c0a176c722f08f3afb7
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2023-10-15 01:18:13 -0300

    test de jugadores y de propietarios, tap me jode

--- api/test/routes/jugadores/index.test.js
+++ api/test/routes/jugadores/index.test.js
@@ -1,5 +1,6 @@
 import { test } from 'tap';
 import { build } from '../../helper.js';
+import { begin, rollback } from '../../../utils/dbNormalization.js';
 
 test("GET de todos los jugadores", async (t) => {
     const app = await build(t);
@@ -29,4 +30,134 @@ test("GET de un solo jugador que no existe", async (t) => {
         method: 'GET'
     });
     t.equal(res.statusCode, 404);
+});
+
+test("POST de un jugador funciona", async (t) => {
+    const app = await build(t);
+    await begin();
+
+    await app.inject({
+        url: '/direcciones',
+        method: 'POST',
+        payload: {
+            pais: "Argentina",
+            estado: "Buenos Aires",
+            ciudad: "La Plata",
+            calle: "Calle de prueba",
+            numero: 123
+        }
+    })
+
+    await app.inject({
+        url: '/telefonos',
+        method: 'POST',
+        payload: {
+            codpais: "54",
+            codarea: "221",
+            numero: "1234567"
+        }
+    })
+
+    await app.inject({
+        url: '/usuarios',
+        method: 'POST',
+        payload: {
+            nombre: "Usuario de prueba",
+            apellido: "Apellido de prueba",
+            fechanac: "2020-01-01",
+            telefonoid: 1,
+            direccionid: 1
+        }
+    })
+
+    const res = await app.inject({
+        url: '/jugadores',
+        method: 'POST',
+        payload: {
+            usuarioid: 1
+        }
+    });
+
+    t.teardown(async () => {
+        await rollback();
+    });
+
+    t.equal(res.statusCode, 200);
+});
+
+test("POST de un jugador con un usuario que no existe", async (t) => {
+    const app = await build(t);
+    await begin();
+
+    const res = await app.inject({
+        url: '/jugadores',
+        method: 'POST',
+        payload: {
+            usuarioid: -10
+        }
+    });
+
+    t.teardown(async () => {
+        await rollback();
+    });
+
+    t.equal(res.statusCode, 500);
+});
+
+test("DELETE de un jugador funciona", async (t) => {
+    const app = await build(t);
+    await begin();
+
+    await app.inject({
+        url: '/direcciones',
+        method: 'POST',
+        payload: {
+            pais: "Argentina",
+            estado: "Buenos Aires",
+            ciudad: "La Plata",
+            calle: "Calle de prueba",
+            numero: 123
+        }
+    })
+
+    await app.inject({
+        url: '/telefonos',
+        method: 'POST',
+        payload: {
+            codpais: "54",
+            codarea: "221",
+            numero: "1234567"
+        }
+    })
+
+    await app.inject({
+        url: '/usuarios',
+        method: 'POST',
+        payload: {
+            nombre: "Usuario de prueba",
+            apellido: "Apellido de prueba",
+            fechanac: "2020-01-01",
+            telefonoid: 1,
+            direccionid: 1
+        }
+    })
+
+    await app.inject({
+        url: '/jugadores',
+        method: 'POST',
+        payload: {
+            usuarioid: 1
+        }
+    });
+
+    const res = await app.inject({
+        url: '/jugadores/1',
+        method: 'DELETE'
+    });
+
+    t.teardown(async () => {
+        await rollback();
+    });
+
+    t.equal(res.statusCode, 200);
 });
--- api/test/routes/propietarios/index.test.js
+++ api/test/routes/propietarios/index.test.js
@@ -1,5 +1,6 @@
 import { test } from 'tap';
 import { build } from '../../helper.js';
+import { begin, rollback } from '../../../utils/dbNormalization.js';
 
 test("GET de todos los propietarios", async (t) => {
     const app = await build(t);
@@ -32,4 +33,134 @@ test("GET de un propietario que no existe", async (t) => {
     });
 
     t.equal(res.statusCode, 404);
+});
+
+test("POST de un propietario funciona", async (t) => {
+    const app = await build(t);
+    await begin();
+
+    await app.inject({
+        url: '/direcciones',
+        method: 'POST',
+        payload: {
+            pais: "Argentina",
+            estado: "Buenos Aires",
+            ciudad: "La Plata",
+            calle: "Calle de prueba",
+            numero: 123
+        }
+    })
+
+    await app.inject({
+        url: '/telefonos',
+        method: 'POST',
+        payload: {
+            codpais: "54",
+            codarea: "221",
+            numero: "1234567"
+        }
+    })
+
+    await app.inject({
+        url: '/usuarios',
+        method: 'POST',
+        payload: {
+            nombre: "Usuario de prueba",
+            apellido: "Apellido de prueba",
+            fechanac: "2020-01-01",
+            telefonoid: 1,
+            direccionid: 1
+        }
+    })
+
+    const res = await app.inject({
+        url: '/propietarios',
+        method: 'POST',
+        payload: {
+            usuarioid: 1
+        }
+    });
+
+    t.teardown(async () => {
+        await rollback();
+    });
+
+    t.equal(res.statusCode, 200);
+});
+
+test("POST de un propietario con un usuario que no existe", async (t) => {
+    const app = await build(t);
+    await begin();
+
+    const res = await app.inject({
+        url: '/propietarios',
+        method: 'POST',
+        payload: {
+            usuarioid: -1
+        }
+    });
+
+    t.teardown(async () => {
+        await rollback();
+    });
+
+    t.equal(res.statusCode, 400);
+});
+
+test("DELETE de un propietario funciona", async (t) => {
+    const app = await build(t);
+    await begin();
+
+    await app.inject({
+        url: '/direcciones',
+        method: 'POST',
+        payload: {
+            pais: "Argentina",
+            estado: "Buenos Aires",
+            ciudad: "La Plata",
+            calle: "Calle de prueba",
+            numero: 123
+        }
+    })
+
+    await app.inject({
+        url: '/telefonos',
+        method: 'POST',
+        payload: {
+            codpais: "54",
+            codarea: "221",
+            numero: "1234567"
+        }
+    })
+
+    await app.inject({
+        url: '/usuarios',
+        method: 'POST',
+        payload: {
+            nombre: "Usuario de prueba",
+            apellido: "Apellido de prueba",
+            fechanac: "2020-01-01",
+            telefonoid: 1,
+            direccionid: 1
+        }
+    })
+
+    await app.inject({
+        url: '/propietarios',
+        method: 'POST',
+        payload: {
+            usuarioid: 1
+        }
+    });
+
+    const res = await app.inject({
+        url: '/propietarios/1',
+        method: 'DELETE'
+    });
+
+    t.teardown(async () => {
+        await rollback();
+    });
+
+    t.equal(res.statusCode, 200);
 });
