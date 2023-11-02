faf0bb9df7c46678fa89f54e426156eab37f78e7
Facundo Martínez <facusml16@gmail.com>
2023-11-02 02:48:38 -0300

    FUNCIONA RUTA LOGIN, retorna token con claims de id y rol de usuario, falta interceptarlo y guardarlo en el frontend

--- api/routes/login/index.js
+++ api/routes/login/index.js
@@ -1,4 +1,5 @@
-//Un schema para hacer prueba del login, cambaiarlo después (modificar las entidades quizá)
+import { query } from '../../db/index.js';
+//Un schema básico por el momento
 const loginSchema = {
     body: {
         type: 'object',
@@ -15,16 +16,34 @@ export default async function(fastify, opts){
     fastify.post('/', { 
         schema: loginSchema,
     }, async function (req, reply) {
-        const { username, password } = req.body;
-        //Aca en vez de esto debería ir una consulta a la BD que pida los datos del usuario y vea si coincide la contraseña
-        //por ahora hardcodeado para probar que funciona
-        if (username != 'Aprobado' || password != 'Aprobado') {
-            return reply.status(401).send({ message: 'Credenciales incorrectas' });
+        try {
+            const { username, password } = req.body;
+            /*
+            Horrible query porque no lo pensamos así, podríamos cambiar para que usuarios tenga campo Rol que sea
+            una FK a una tabla separada Roles con los distintos roles hardcodeados, y con endpoints sólo autorizados 
+            para un admin, para la próxima ya aprendimos al menos... O lo cambiamos y fixeamos todos los tests que se rompan.
+            */
+            const res = await query(
+            `
+            SELECT u.id, 
+                CASE 
+                    WHEN j.usuarioid IS NOT NULL THEN 'jugador'
+                    WHEN p.usuarioid IS NOT NULL THEN 'propietario'
+                    ELSE 'administrador'
+                END AS role
+            FROM usuarios u
+            LEFT JOIN jugadores j ON u.id = j.usuarioid
+            LEFT JOIN propietarios p ON u.id = p.usuarioid
+            where u.username = $1 and u.password = crypt($2, u.password);
+            `, [username, password]);
+            if (res.rows.length === 0) return reply.status(401).send({ message: 'Credenciales incorrectas' });
+            const { id, role } = res.rows[0];
+            const token = fastify.jwt.sign({id, role}, {
+                expiresIn: '1h'
+            });
+            return reply.status(201).send(token);
+        } catch (error) {
+            return reply.status(500).send({ message: error });
         }
-        
-        const token = fastify.jwt.sign({ username }, {
-            expiresIn: '1h'
-        });
-        return reply.status(201).send(token);
     });
 }
