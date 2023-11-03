83844a03626bcce57c78b4996dda0695fe03c629
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2023-11-02 23:05:28 -0300

    mila de pollo

--- frontend/src/app/shared/components/user/user.component.ts
+++ frontend/src/app/shared/components/user/user.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
-  username = 'Contoso';
+  username = 'miladepollo123';
   pictureurl = 'https://picsum.photos/200/300';
 }
