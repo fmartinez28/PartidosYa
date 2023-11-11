c74da364edfc8ba97bb85d8d18e752b4459ebecc
Facundo Martínez <facusml16@gmail.com>
2023-11-11 17:51:02 -0300

    Añadida página básica para visualizar los partidos públicos disponibles, añadido componente reutilizable partido

--- frontend/src/app/main/main-routing.module.ts
+++ frontend/src/app/main/main-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { CanchasPlaceholderComponent } from './components/canchas-placeholder/canchas-placeholder.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { isUserLogged, isUserNotLogged } from '../session/guards/auth.guard';
+import { PartidosComponent } from './components/partidos/partidos.component';
 
 const routes: Routes = [{
   path: '',
@@ -14,23 +15,28 @@ const routes: Routes = [{
       path: 'home',
       component: HomeComponent,
     },
-  
+
     {
       path: 'session',
       loadChildren: () => import('../session/session.module').then(m => m.SessionModule),
       canMatch: [isUserNotLogged]
     },
-  
+
     {
       path: 'canchas',
       component: CanchasPlaceholderComponent, // TODO hacer que no se pueda entrar a este si no hay login
       canMatch: [isUserLogged]
     },
-  
+    {
+      path: 'partidos',
+      component: PartidosComponent,
+      canMatch: [isUserLogged]
+    },
+
     { path: 'login', redirectTo: 'session/login' },
     { path: 'signup', redirectTo: 'session/signup' },
     { path: 'notfound', component: NotFoundComponent },
-  
+
     { path: '', redirectTo: 'home', pathMatch: 'full' },
   ]}
 ];
