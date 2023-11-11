b86cea10cb7503c800ca8000964b64d26e2d7414
Facundo Martínez <facusml16@gmail.com>
2023-11-11 17:51:02 -0300

    Añadida página básica para visualizar los partidos públicos disponibles, añadido componente reutilizable partido

--- frontend/src/app/main/components/partido/partido.component.ts
+++ frontend/src/app/main/components/partido/partido.component.ts
@@ -1,3 +1,4 @@
+import { Time } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./partido.component.scss']
 })
 export class PartidoComponent {
+  public playerCount: number = 0;
+  public playerLimit: number = 10;
+  public scheduledDate?: Date = new Date(2023, 11, 22, 18, 15);
 
+  constructor(){}
+  public pitch: string = 'María Auxiliadora';
+  //Esto quizá hacer primero un fetch de todos los partidos a los que el usuario pertenece primero
+  public playerHasJoined: boolean = true;
+  public community: string = 'PartidosYaTeam';
 }
