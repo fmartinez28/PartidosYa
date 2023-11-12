48c9de281db7f0973a8c4338a50e91ca054fada7
Tomás Esteves <estevestomas27@gmail.com>
2023-11-12 17:40:08 -0300

    ahora si fix sql

--- api/db/crearTablas.sql
+++ api/db/crearTablas.sql
@@ -10,28 +10,9 @@ DROP TABLE IF EXISTS usuarios CASCADE;
 DROP TABLE IF EXISTS telefonos CASCADE;
 DROP TABLE IF EXISTS direcciones CASCADE;
 DROP TABLE IF EXISTS roles CASCADE;
-DROP TABLE IF EXISTS comunidadmoderador CASCADE;
-DROP TABLE IF EXISTS comunidadjugador CASCADE;
-DROP TABLE IF EXISTS participacionpartido CASCADE;
-DROP TABLE IF EXISTS partido CASCADE;
-DROP TABLE IF EXISTS canchas CASCADE;
-DROP TABLE IF EXISTS comunidades CASCADE;
-DROP TABLE IF EXISTS propietarios CASCADE;
-DROP TABLE IF EXISTS jugadores CASCADE;
-DROP TABLE IF EXISTS usuarios CASCADE;
-DROP TABLE IF EXISTS telefonos CASCADE;
-DROP TABLE IF EXISTS direcciones CASCADE;
-DROP TABLE IF EXISTS roles CASCADE;
 DROP EXTENSION IF EXISTS pgcrypto;
 
 CREATE EXTENSION pgcrypto;
-
-CREATE TABLE public.roles (
-	id serial4 NOT NULL,
-	nombre varchar NOT NULL,
-	CONSTRAINT roles_pk PRIMARY KEY (id),
-	CONSTRAINT roles_un UNIQUE (nombre)
-);
 
 CREATE TABLE public.roles (
 	id serial4 NOT NULL,
@@ -65,18 +46,12 @@ CREATE TABLE public.usuarios (
 	email varchar NOT NULL,
 	username varchar NOT NULL,
 	"password" text NOT NULL,
-	email varchar NOT NULL,
-	username varchar NOT NULL,
-	"password" text NOT NULL,
 	fechanac date NOT NULL,
 	telefonoid int8 NOT NULL,
 	direccionid int8 NOT NULL,
 	rolid int8 NOT NULL,
 	CONSTRAINT usuarios_email_key UNIQUE (email),
-	rolid int8 NOT NULL,
-	CONSTRAINT usuarios_email_key UNIQUE (email),
 	CONSTRAINT usuarios_pk PRIMARY KEY (id),
-	CONSTRAINT usuarios_username_key UNIQUE (username),
 	CONSTRAINT usuarios_username_key UNIQUE (username),
 	CONSTRAINT usuarios_direccionid_fkey FOREIGN KEY (direccionid) REFERENCES public.direcciones(id),
 	CONSTRAINT usuarios_rolid_fk FOREIGN KEY (rolid) REFERENCES public.roles(id),
@@ -108,11 +83,8 @@ CREATE TABLE public.partido (
 	canchaid int8 NOT NULL,
 	playerscount int NOT NULL default 0,
 	playerslimit int NOT NULL default 10,
-	playerscount int NOT NULL default 0,
-	playerslimit int NOT NULL default 10,
 	fechacreacion date NOT NULL,
 	fechaprogramada date NOT NULL,
-	comunidadid int8 NULL,
 	comunidadid int8 NULL,
 	CONSTRAINT partido_pk PRIMARY KEY (id),
 	CONSTRAINT partido_fk FOREIGN KEY (canchaid) REFERENCES public.canchas(id),
@@ -197,66 +169,6 @@ EXECUTE FUNCTION decrementar_playerscount();
 INSERT INTO roles(nombre) VALUES('jugador');
 INSERT INTO roles(nombre) VALUES('propietario');
 INSERT INTO roles(nombre) VALUES('administrador');
-CREATE TABLE public.comunidadmoderador (
-	comunidadid int8 NOT NULL,
-	usuarioid int8 NOT NULL,
-	CONSTRAINT comunidadmoderador_fk FOREIGN KEY (comunidadid) REFERENCES public.comunidades(id),
-	CONSTRAINT comunidadmoderador_fk_1 FOREIGN KEY (usuarioid) REFERENCES public.usuarios(id)
-);
-
--- FUNCIONES Y TRIGGERS PARA MANEJO DEL CONTADOR DE MIEMBROS EN UNA COMUNIDAD --
-CREATE OR REPLACE FUNCTION incrementar_memberscount() RETURNS TRIGGER AS $$
-BEGIN
-    UPDATE comunidades SET memberscount = memberscount + 1 WHERE id = NEW.comunidadid;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER incrementar_memberscount_trigger
-AFTER INSERT ON comunidadjugador
-FOR EACH ROW
-EXECUTE FUNCTION incrementar_memberscount();
-
-CREATE OR REPLACE FUNCTION decrementar_memberscount() RETURNS TRIGGER AS $$
-BEGIN
-    UPDATE comunidades SET memberscount = memberscount - 1 WHERE id = OLD.comunidadid;
-    RETURN OLD;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER decrementar_memberscount_trigger
-AFTER DELETE ON comunidadjugador
-FOR EACH ROW
-EXECUTE FUNCTION decrementar_memberscount();
-
--- FUNCIONES Y TRIGGERS PARA MANEJO DEL CONTADOR DE MIEMBROS DE UN PARTIDO --
-CREATE OR REPLACE FUNCTION incrementar_playerscount() RETURNS TRIGGER AS $$
-BEGIN
-    UPDATE partido SET playerscount = playerscount + 1 WHERE id = NEW.partidoid;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER incrementar_playerscount_trigger
-AFTER INSERT ON participacionpartido
-FOR EACH ROW
-EXECUTE FUNCTION incrementar_playerscount();
-
-CREATE OR REPLACE FUNCTION decrementar_playerscount() RETURNS TRIGGER AS $$
-BEGIN
-    UPDATE partido SET playerscount = playerscount - 1 WHERE id = OLD.partidoid;
-    RETURN OLD;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER decrementar_contador_partidos_trigger
-AFTER DELETE ON participacionpartido
-FOR EACH ROW
-EXECUTE FUNCTION decrementar_playerscount();
-
-INSERT INTO roles(nombre) VALUES('jugador');
-INSERT INTO roles(nombre) VALUES('propietario');
-INSERT INTO roles(nombre) VALUES('administrador');
 INSERT INTO direcciones(pais, estado, ciudad, calle, numero) VALUES('Uruguay', 'Salto', 'Salto', 'Uruguay', '911');
 INSERT INTO direcciones(pais, estado, ciudad, calle, numero) VALUES('Uruguay', 'Montevideo', 'Cerro', 'Cerrito', '922');
 INSERT INTO telefonos(codpais, codarea, numero) VALUES('+598', '473', '911');
@@ -265,16 +177,9 @@ INSERT INTO usuarios(rolid, nombre, apellido, username, fechanac, telefonoid, di
 INSERT INTO usuarios(rolid, nombre, apellido, username, fechanac, telefonoid, direccionid, email, "password") VALUES(1, 'Tyranno', 'Saurus', 'jugador2','1960-11-10', 1, 1, 'someone@example.com', crypt('admin', gen_salt('bf')));
 INSERT INTO usuarios(rolid, nombre, apellido, username, fechanac, telefonoid, direccionid, email, "password") VALUES(2, 'Rico', 'McRico', 'propietario1','1960-11-10', 1, 1, 'alguien@empresa.com', crypt('admin', gen_salt('bf')));
 INSERT INTO usuarios(rolid, nombre, apellido, username, fechanac, telefonoid, direccionid, email, "password") VALUES(2, 'Humilde', 'McCristiano', 'propietario2','1960-11-10', 1, 1, 'otro@empresa.com', crypt('admin', gen_salt('bf')));
-INSERT INTO usuarios(rolid, nombre, apellido, username, fechanac, telefonoid, direccionid, email, "password") VALUES(1, 'Stego', 'Saurus', 'jugador1', '1970-10-01', 1, 1, 'alguien@example.com', crypt('admin', gen_salt('bf')));
-INSERT INTO usuarios(rolid, nombre, apellido, username, fechanac, telefonoid, direccionid, email, "password") VALUES(1, 'Tyranno', 'Saurus', 'jugador2','1960-11-10', 1, 1, 'someone@example.com', crypt('admin', gen_salt('bf')));
-INSERT INTO usuarios(rolid, nombre, apellido, username, fechanac, telefonoid, direccionid, email, "password") VALUES(2, 'Rico', 'McRico', 'propietario1','1960-11-10', 1, 1, 'alguien@empresa.com', crypt('admin', gen_salt('bf')));
-INSERT INTO usuarios(rolid, nombre, apellido, username, fechanac, telefonoid, direccionid, email, "password") VALUES(2, 'Humilde', 'McCristiano', 'propietario2','1960-11-10', 1, 1, 'otro@empresa.com', crypt('admin', gen_salt('bf')));
 INSERT INTO comunidades(id, nombre) VALUES(1, 'PruebaCom');
-INSERT INTO comunidadmoderador(usuarioid, comunidadid) VALUES(1, 1);
 INSERT INTO comunidadmoderador(usuarioid, comunidadid) VALUES(1, 1);
 INSERT INTO comunidadjugador(jugadorid, comunidadid, fecharegistro) VALUES(1, 1, '2010-05-10'); 
 INSERT INTO comunidadjugador(jugadorid, comunidadid, fecharegistro) VALUES(2, 1, '2012-08-02');
-INSERT INTO canchas(nombre, direccionid, canchanum, propietarioid) VALUES('Cancha 1', 1, 1, 3);
-INSERT INTO canchas(nombre, direccionid, canchanum, propietarioid) VALUES('Cancha 2', 2, 2, 4);
 INSERT INTO canchas(nombre, direccionid, canchanum, propietarioid) VALUES('Cancha 1', 1, 1, 3);
 INSERT INTO canchas(nombre, direccionid, canchanum, propietarioid) VALUES('Cancha 2', 2, 2, 4);
