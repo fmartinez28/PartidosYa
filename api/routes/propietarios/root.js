6ce018dfaf9cb41d9259818139cd29085967de34
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2023-11-15 22:55:31 -0300

    arreglado y mejorado endpoint de propietarios

--- api/routes/propietarios/root.js
+++ api/routes/propietarios/root.js
@@ -1,11 +1,12 @@
 import { query } from "../../db/index.js";
 import * as ownersSchemas from '../../schemas/propietarios/root.js';
+import * as canchasSchemas from '../../schemas/canchas/root.js';
 
 export default async function (fastify, opts) {
     fastify.get('/', { schema: ownersSchemas.getAllSchema }, async function (request, reply) {
         // top 10 queries de los videojuegos
         // debería tomar todos los usuarios que sean propietarios
-        const queryresult = await query('SELECT usuarios.* FROM usuarios JOIN propietarios ON usuarios.id = propietarios.usuarioid');
+        const queryresult = await query('SELECT * FROM usuarios where rolid = 2');
         const rows = queryresult.rows;
         if (rows.length === 0)
             return reply.status(204).send({ message: 'No se encontraron propietarios' });
@@ -14,34 +15,18 @@ export default async function (fastify, opts) {
 
     // seleccionar de la tabla propietarios el propietario que coincida la ID. Seleccionar de la tabla usuarios el usuario que coincida la ID. Retornar el Usuario.
     fastify.get('/:id', { schema: ownersSchemas.getByIdSchema }, async function (request, reply) {
-        const queryresult = await query('SELECT * FROM "propietarios" WHERE "usuarioid" = $1', [request.params.id]);
+        const queryresult = await query('SELECT * FROM "usuarios" WHERE "id" = $1 and rolid = 2', [request.params.id]);
         const rows = queryresult.rows;
         if (rows.length === 0)
-            return reply.status(404).send({ error: 'Propietario no encontrado' });
-        const usuarioQueryResult = await query('SELECT * FROM "usuarios" WHERE "id" = $1', [rows[0].usuarioid]);
-        const rowsUsuario = usuarioQueryResult.rows;
-        if (rowsUsuario.length === 0)
-            return reply.status(404).send({ error: 'No se encontró un usuario cuyo Id corresponda al Id del propietario solicitado' });
-        return reply.status(200).send(rowsUsuario[0]);
+            return reply.status(404).send({ message: 'No se encontró un usuario con ese id o el usuario no correspondía a un propietario.' });
+        return reply.status(200).send(rows[0]);
     });
 
-    fastify.post('/', { schema: ownersSchemas.postSchema }, async function (request, reply) {
-        const { usuarioid } = request.body;
-        const queryresult = await query('INSERT INTO "propietarios" ("usuarioid") VALUES ($1) RETURNING *', [usuarioid]);
-        if (queryresult.rows.length === 0)
-            return reply.status(500).send({ message: 'Error al crear el propietario' });
-        return reply.status(201).send(queryresult.rows[0]);
-    });
-
-    fastify.delete('/:id', { schema: ownersSchemas.deleteSchema }, async function (request, reply) {
-        const id = request.params.id;
-        try {
-            const queryresult = await query('DELETE FROM "propietarios" WHERE "usuarioid" = $1 RETURNING *', [id]);
-            if (queryresult.rows.length === 0)
-                return reply.status(404).send({ message: 'Propietario no encontrado' });
-            return reply.status(200).send({ message: "El propietario fue eliminado" });
-        } catch (error) {
-            return reply.status(500).send({ message: error });
-        }
+    fastify.get('/:id/canchas', { schema: canchasSchemas.getAllSchema }, async function (request, reply) {
+        const queryresult = await query('SELECT * FROM "canchas" WHERE "propietarioid" = $1', [request.params.id]);
+        const rows = queryresult.rows;
+        if (rows.length === 0)
+            return reply.status(204).send({ message: `No se encontraron canchas para el propietario ` });
+        return reply.send(queryresult.rows);
     });
 }
